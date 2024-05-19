38e3609997f90757442b24cca8ce8083d5b1583e
Dmytro Zanuda <dmzanuda.work@gmail.com>
2024-05-19 14:30:10 +0300

    Revert "init using clean architecture, ddd, add start migration, test mediator + api"
    This reverts commit 0984b14d97ce769e904e76d3571dd3f601f16572.

--- FilmLib.Application/FilmActions/Queries/GetFilms/GetFilmsQuery.cs
+++
@@ -1,6 +0,0 @@
-using FilmLib.Application.Messaging;
-using FilmLib.Domain.Models;
-
-namespace FilmLib.Application.FilmActions.Queries.GetFilms;
-
-public class GetFilmsQuery : IQuery<List<Film>>;
--- FilmLib.Application/Messaging/IQuery.cs
+++
@@ -1,8 +0,0 @@
-using CSharpFunctionalExtensions;
-using MediatR;
-
-namespace FilmLib.Application.Messaging;
-
-public interface IQuery<TResponse> : IRequest<Result<TResponse>>
-{
-}
